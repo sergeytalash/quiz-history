9fe2f0b1650ef3c0093064a525aa20a4ef591b71
sergey.talash <lasmor@inbox.ru>
2022-09-13 17:07:30 +0600

    Questions splited by 50. Used buttons instead of selectbox.

--- main.py
+++ main.py
@@ -14,19 +14,24 @@ class Main:
                 st.session_state.answers = json.load(fr)
         st.session_state.setdefault('correct_answers', {})
         st.session_state.setdefault('letters', {1: "А", 2: "Б", 3: "В"})
+        st.session_state.setdefault('selected_page', sorted(st.session_state['questions'].keys())[0])
 
     @staticmethod
     def answer(current, valid, question_index):
         st.session_state.correct_answers[question_index] = current == valid
 
     @staticmethod
-    def min_max(qa_dict):
+    def select_topic(name):
+        st.session_state['selected_page'] = name
+
+    @staticmethod
+    def min_max_question_number(qa_dict):
         qs = [int(i) for i in qa_dict.keys()]
         return f"{min(qs)}-{max(qs)}"
 
     def get_page(self, topic):
         qa_dict = st.session_state['questions'][topic]
-        st.markdown(f"## {topic} {self.min_max(qa_dict)}")
+        st.markdown(f"## {topic} {self.min_max_question_number(qa_dict)}")
         for i, qa in qa_dict.items():
             st.markdown(f"#### Вопрос {i}: {qa['q']}")
             [st.button(label=f"{st.session_state.letters[v]}) {qa[str(v)]}",
@@ -37,8 +42,12 @@ class Main:
                 st.success("Верно")
 
     def run(self):
-        selected_page = st.sidebar.selectbox("Выбрать тему:", st.session_state['questions'].keys())
-        self.get_page(selected_page)
+        for topic in sorted(st.session_state['questions'].keys()):
+            st.sidebar.button(label=topic,
+                              key=uuid.uuid4(),
+                              on_click=self.select_topic,
+                              args=(topic,))
+        self.get_page(st.session_state['selected_page'])
 
 
 if __name__ == '__main__':
